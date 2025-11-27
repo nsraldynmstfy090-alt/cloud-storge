aa09210090e45638ea534bd9cdbdb876013102f7
nsraldynmstfy090-alt <nsraldynmstfy090@gmail.com>
2025-11-27 11:56:23 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> from flask import Flask, request, jsonify
+
 ... import subprocess, uuid, os
 ... 
 ... app = Flask(__name__)
@@ -33,3 +31,4 @@ Enter "help" below or click "Help" above for more information.
 ... 
 ... 
 ... app.run(host="0.0.0.0", port=5000)
+
